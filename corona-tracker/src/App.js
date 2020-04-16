32c18d40b1c974b2b4652984c4f2ffcaff63182a
lopeselio <lopeselio@gmail.com>
2020-04-16 17:56:28 +0530

    imports

--- corona-tracker/src/App.js
+++ corona-tracker/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Cards from './components/Cards'
+import Chart from './components/Chart'
+import Country from './components/CountrySelector'
+
 
 class App extends React.Component {
   render () {
