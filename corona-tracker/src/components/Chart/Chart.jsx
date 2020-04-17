fc9e915754cf4ed7043318964ddbd989b32e8d41
lopeselio <lopeselio@gmail.com>
2020-04-17 17:58:10 +0530

    barChart

--- corona-tracker/src/components/Chart/Chart.jsx
+++ corona-tracker/src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2'
 
 import styles from './Chart.module.css'
 
-const Charts = () => {
+const Charts = ({ data: { confirmed, deaths, recovered }, country }) => {
   const [dailyData, setDailyData] = useState([])
   useEffect(() => {
     const fetchAPI = async () => {
@@ -34,9 +34,34 @@ const Charts = () => {
         }}
         />) : null
   )
+
+  const barChart = (
+    confirmed
+      ? (
+        <Bar
+          data={{
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [{
+              label: 'People',
+              backgroundColor: [
+                'rgba(0,0,255,0.5)',
+                'rgba(0,255,0,0.5)',
+                'rgba(255,0,0,0.5)'
+              ],
+              data: [confirmed, recovered, deaths]
+            }]
+
+          }}
+          options={{
+            legend: { display: false },
+            title: { display: true, text:`Current scenario in ${country}`}
+          }}
+        />
+      ) : null
+  )
   return (
     <div className={styles.container}>
-      {lineChart}
+      {country ? barChart : lineChart}
     </div>
   )
 }
