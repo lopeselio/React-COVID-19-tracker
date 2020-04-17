5c74fed0c1b24f03bc17530ae09950a775355b8f
lopeselio <lopeselio@gmail.com>
2020-04-17 15:10:15 +0530

    added charts and making linechart

--- corona-tracker/src/api/index.js
+++ corona-tracker/src/api/index.js
@@ -11,3 +11,10 @@ export const fetchData = async () => {
   } catch (error) {
   }
 }
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`)
+    console.log(data)
+  } catch (error) {
+  }
+}
--- corona-tracker/src/components/Chart/Chart.jsx
+++ corona-tracker/src/components/Chart/Chart.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from '../../api'
+import { Line, Bar } from 'react-chartjs-2'
+
+import styles from './Chart.module.css'
 
 const Charts = () => {
+  const [dailyData, setDailyData] = useState({})
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData())
+    }
+    console.log(dailyData)
+    fetchAPI()
+  })
+
+  const lineChart = (
+    dailyData[0]
+      ? (
+        <Line data={{
+          labels: dailyData(({ date }) => date),
+          datasets: [{
+            
+          }]
+        }}
+        />) : null
+  )
   return (
     <h1>Charts</h1>
   )
