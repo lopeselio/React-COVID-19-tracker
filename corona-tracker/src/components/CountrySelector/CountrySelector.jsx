30a555cff3e47d1ed1bc80f8564fbb3803e63fac
lopeselio <lopeselio@gmail.com>
2020-04-17 16:09:23 +0530

    useEffect fetchCounties async callback

--- corona-tracker/src/components/CountrySelector/CountrySelector.jsx
+++ corona-tracker/src/components/CountrySelector/CountrySelector.jsx
@@ -3,7 +3,13 @@ import { NativeSelect, FormControl, StylesProvider } from '@material-ui/core'
 import styles from './CountrySelector.module.css'
 import { countries } from '../../api/index'
 const Country = () => {
-  
+  const [ fetchCountries, setFetchedCountries ] = useState([])
+  useEffect(( => {
+    const fetchCountries = async () => {
+      setFetchedCountries(await countries)
+    }
+    fetchCountries()
+  }))
   return (
     <FormControl className={Styles.FormControl}>
       <NativeSelect>
