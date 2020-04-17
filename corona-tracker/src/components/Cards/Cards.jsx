439298005da115959b610d64a41d21c59259994d
lopeselio <lopeselio@gmail.com>
2020-04-17 14:27:38 +0530

    card css fixes

--- corona-tracker/src/components/Cards/Cards.jsx
+++ corona-tracker/src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
-import styles from './Cards.modules.css'
+import styles from './Cards.module.css'
 import CountUp from 'react-countup'
 import cx from 'classnames'
 
