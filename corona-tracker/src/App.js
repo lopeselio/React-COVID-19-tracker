f6048ee125f29e5c01576cf19b7194402a430aed
lopeselio <lopeselio@gmail.com>
2020-04-17 11:18:10 +0530

    fetching data from api

--- corona-tracker/src/App.js
+++ corona-tracker/src/App.js
@@ -5,9 +5,22 @@ import React from 'react'
 
 import { Cards, Country, Charts } from './components'
 import styles from './App.module.css'
+import { fetchData } from './api/index'
 
 class App extends React.Component {
+state = {
+  data: {},
+}
+
+  async componentDidMount () {
+    const fetchedData = await fetchData()
+    this.setState({ data: fetchedData })
+    // console.log(data)
+  }
+
   render () {
+    const { data } = this.state
+    
     return (
       <div className={styles.container}>
         <Cards />
